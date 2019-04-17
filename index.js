4441a60081694f996af8a75c774fcd90070ceb7d
nirvanarockk@hotmail.com <nirvanarockk@hotmail.com>
2019-04-17 12:07:45 -0500

    Indentasion changes

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-module.exports = () => {
-  // ...
+#!/usr/bin/env node
+
+const mdLinks = require('../GDL002-md-links/blob/master/README').mdLinks;
+const path = require('path');
+const fetch = require('node-fetch');
+const [, , ...args] = process.argv;
+
+if (require.main === module) {
+  let options = {};
+  if (process.argv.includes('--validate')) options.validate = true;
+  if (process.argv.includes('--stats')) options.stats = true;
+  mdLinks(path.join(process.cwd(), args[0]), options).then((links) => {
+    let result = '';
+    let successCounter = 0;
+    let failCounter = 0;
+    links.map(element => {
+      fetch(element.href)
+        .then(res => {
+          if (options.validate) {
+            result = (`${element.file}: ${element.line} - ${(element.href)} - ${element.text} ${(res.status)} ${(res.ok)}`);
+          } else {
+            result = (`${(element.file)}: ${(element.line)} - ${(element.href)} - ${element.text}`);
+          }
+          if (options.stats) {
+            if (res.ok === true) {
+              successCounter++;
+            } else if (res.ok === false) {
+              failCounter++;
+            }
+            stats = (`${('totals: ')} ${(links.filter(link => link.href).length)}, ${('success: ')} ${(successCounter)}, ${('failed: ')} ${(failCounter)}`);
+          }
+          console.log(result);
+          if (options.stats) console.log(stats);
+          console.log('hi there!');
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
 };
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,12 +3,12 @@ const mdLinks = require('../mdlinks');
 
 const folder = '../README.md';
 
-it('mdLinks is an object', () => {
+test('mdLinks is an object', () => {
   expect(typeof mdLinks).toBe('object');
 });
-it('should return false to a relative path', () => {
+test('should return false to a relative path', () => {
   expect(mdLinks.isAbsolute(folder)).toBeFalsy();
 });
-it('shoul return the file extension', () => {
+test('should return the file extension', () => {
   expect(mdLinks.checkExtName(folder)).toBe('.md');
 });
